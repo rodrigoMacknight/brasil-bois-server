9d29f6fc950d2ef6b2d560f61aea579f4281d93b
t733984 <rodrigo.poloni@santander.com.br>
2021-06-20 19:38:09 -0300

    feature/fullgame

--- index.js
+++ index.js
@@ -6,16 +6,46 @@ server.listen(8080, function() {
         console.log("server is now running");
 });
 
-
+//configure sockets for two players
 io.on('connection', function(socket) {
     console.log("player connected");
+    //recebeu um evento de carta colocada do player, manda para outro player o evento de carta
+    //colocada
+	socket.emit('socketId', {id: socket.id});
 
 
+    socket.broadcast.emit('newPlayer', {id: socket.id});
+
+
+    //coloca a carta do player no board do inimigo
     socket.on('placeCard', function(resp){
-		console.log(resp);
-		socket.emit('enemyCardPlaced', resp);
-		
-	});
+        console.log(resp);
+        socket.broadcast.emit('enemyCardPlaced', resp);
+        
+    });
+
+    socket.on('endTurn', function(resp) {
+        console.log("endTurn: " + resp);
+        socket.broadcast.emit('endEnemyTurn')
+    });
+
+    socket.on('playerFound', function(resp) {
+        console.log("playerFound: ");
+        socket.broadcast.emit('playerFound');
+    });
+
+    socket.on('attackCreature', function(resp){
+        console.log(resp);
+        socket.broadcast.emit('enemyAttackedCreature', resp);
+        
+    });
+    
+
+    socket.on('attackHp', function(resp){
+        console.log(resp);
+        socket.broadcast.emit('enemyAttackedHp', resp);
+        
+    });
 
     socket.on('disconnect', function() {
         console.log("player disconected");
